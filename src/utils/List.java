bbb7f1a505accc4af028b7173f052beb7a085d2c
Roberto Bolanos <roberto_bolanos@icloud.com>
2025-03-07 19:53:28 -0800

    Added List interface

---
+++ src/utils/List.java
@@ -0,0 +1,19 @@
+package utils;
+
+import java.util.List;
+
+public interface List<E> {
+    boolean add(E item);
+    void add(int index, E item);
+    boolean addAll(List<E> otherList);
+    void clear();
+    boolean contains(E item);
+    E get(int index);
+    int indexOf(E item);
+    boolean isEmpty();
+    E remove(int index);
+    boolean remove(E item);
+    boolean removeAll(List<E> otherList);
+    E set(int index, E item);
+    int size();
+}
